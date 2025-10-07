f470af3f5e77dd8ace369cb59e85cf1ae2b4f5a8
nelsonortfan <n.ortizf@uniandes.edu.co>
2025-10-07 15:31:09 -0500

    Using Password Enconders to encrypt and hashing passwords

--- springsecsection3/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
+++ springsecsection3/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
@@ -2,13 +2,17 @@ package com.eazybytes.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
 
 @Configuration
 public class ProjectSecurityConfig {
@@ -26,8 +30,14 @@ public class ProjectSecurityConfig {
     @Bean
     public UserDetailsService userDetailsService(){
         UserDetails user = User.withUsername("user").password("{noop}12345").authorities("read").build();
-        UserDetails admin = User.withUsername("admin").password("{noop}54321").authorities("admin").build();
+        UserDetails admin = User.withUsername("admin").password("{bcrypt}$2a$12$tIygJdiw7miv8nzb.tYQB.lDWpQSAJi2VHM.jRrXIpYHQrchkpSmm").authorities("admin").build();
         return new InMemoryUserDetailsManager(user, admin);
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
 
 }

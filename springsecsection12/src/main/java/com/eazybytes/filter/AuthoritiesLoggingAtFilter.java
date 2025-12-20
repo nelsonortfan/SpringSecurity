e6f23b9606dcf5a0b884552f03c835d9309882d9
nelsonortfan <n.ortizf@uniandes.edu.co>
2025-12-20 11:58:42 -0500

    Security Methods base

---
+++ springsecsection12/src/main/java/com/eazybytes/filter/AuthoritiesLoggingAtFilter.java
@@ -0,0 +1,16 @@
+package com.eazybytes.filter;
+
+import jakarta.servlet.*;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+
+@Slf4j
+public class AuthoritiesLoggingAtFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        log.info("Authentication validation in progress");
+    }
+
+}
---
+++ springsecsection12/src/main/java/com/eazybytes/model/Accounts.java
@@ -0,0 +1,30 @@
+package com.eazybytes.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Date;
+
+@Entity
+@Getter
+@Setter
+public class Accounts {
+
+    @Column(name = "customer_id")
+    private long customerId;
+
+    @Id
+    @Column(name = "account_number")
+    private long accountNumber;
+
+    @Column(name="account_type")
+    private String accountType;
+
+    @Column(name = "branch_address")
+    private String branchAddress;
+
+    @Column(name = "create_dt")
+    private Date createDt;
+
+}
